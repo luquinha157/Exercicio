2a50d2b7278effe91f89d98a71befca0416d204c
lucas <0000600853@senaimgaluno.com.br>
2023-09-04 21:25:10 -0300

    Conectei meu template ao banco de dados que eu criei

--- Games/Games/models.py
+++ Games/Games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class PgPrincipal(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    description =  models.TextField()
+    path = models.ImageField(upload_to="images/")
+    preco = models.DecimalField(default=000.00, max_digits= 5, decimal_places=2)
+    date_create = models.DateTimeField(default=datetime.now, blank =True)
--- Games/Games/views.py
+++ Games/Games/views.py
@@ -1,7 +1,19 @@
+from tkinter import Entry
 from django.shortcuts import render
+from Games.models import PgPrincipal
 from django.http import HttpResponse
 
 # Create your views here.
-
 def index(request):
-    return render(request, "index.html")
+    all_products = PgPrincipal.objects.all() 
+    return render(request, "index.html", {"guitarras": all_products})
+
+def test1(request):
+    #all_entries = Site.objects.get(id=1)
+    #resulta = []
+    #for x in all_entries:
+    #    formatador = "x.nome.card+"
+    #    resulta.append()
+    retornar = all_entries.nome_card + " | " + all_entries.description + " | "
+
+    return HttpResponse(retornar)
